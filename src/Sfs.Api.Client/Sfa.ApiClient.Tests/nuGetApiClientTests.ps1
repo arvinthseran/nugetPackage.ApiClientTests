545a00addd803c16751f178d7bf0a254feac8f34
Arvinth Seran <arvinth.seran@gmail.com>
2017-07-11 16:47:25 +0100

    minor changes

--- src/Sfs.Api.Client/Sfa.ApiClient.Tests/nuGetApiClientTests.ps1
+++ src/Sfs.Api.Client/Sfa.ApiClient.Tests/nuGetApiClientTests.ps1
@@ -1,4 +1,4 @@
-$packages="SFA.Roatp.Api.Client"
+$packages="SFA.DAS.Apprenticeships.Api.Client"
 # SFA.Roatp.Api.Client ,SFA.DAS.Apprenticeships.Api.Client,"SFA.DAS.AssessmentOrgs.Api.Client","SFA.DAS.Providers.Api.Client"
 $a = Get-Date
 foreach ($package in $packages) 
@@ -11,7 +11,7 @@ foreach ($version in $providerapiversions)
 $currentTestVersion = $version."Version"
 Write-Host "Testing " -nonewline; Write-Host $package"."$currentTestVersion -foregroundcolor red -backgroundcolor yellow
 
-$outputfile="C:\SFA\nugetPackage.ApiClientTests\src\Sfs.Api.Client\Sfa.ApiClient.Tests\bin\Debug\testouput"+$package+$currentTestVersion+$a.Day+$a.Month+$a.Hour+$a.Minute+".txt"
+$outputfile="C:\SFA\nugetPackage.ApiClientTests\src\Sfs.Api.Client\Sfa.ApiClient.Tests\bin\Debug\testouput"+$package+$currentTestVersion+"-"+$a.Day+$a.Month+$a.Hour+$a.Minute+".txt"
 C:\SFA\nugetPackage.ApiClientTests\src\Sfs.Api.Client\packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe C:\SFA\nugetPackage.ApiClientTests\src\Sfs.Api.Client\Sfa.ApiClient.Tests\bin\Debug\Sfa.ApiClient.Tests.dll --params=version=$currentTestVersion --params=packageinTest=$package --output=$outputfile
 }
 }
